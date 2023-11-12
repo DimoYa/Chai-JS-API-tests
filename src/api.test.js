4e38c05662be2bc8baca08edca59ab74084be149
DimoYa <dimo_yanev@abv.bg>
2023-11-13 00:33:09 +0200

    init and test

---
+++ src/api.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./server');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Books API', () => {
+    it('Should POST a book', (done) => {
+        const book = { id: "1", title: "Test Book", author: "Test Author" };
+        chai.request(server)
+            .post('/books')
+            .send(book)
+            .end((err, res) => {
+                expect(res).to.have.status(201);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('title');
+                expect(res.body).to.have.property('author');
+                book.id = res.body.id;
+                done();
+            });
+    });
+});
